19127676e55198df12608edaa476d8ed57659b83
Rehan Peiris <rehanperis58@gmail.com>
2024-09-05 01:20:40 +0530

    Backend for profiles

---
+++ models/Profile.js
@@ -0,0 +1,24 @@
+const ProfileSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: false,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+})
+
+export default mongoose.model('Profile', ProfileSchema)
