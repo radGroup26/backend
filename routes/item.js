da64898dfc30c9878ae17c3d5a314f4092cf1664
nova9 <thathsaramadhhusha@gmail.com>
2024-09-06 21:21:18 +0530

    chaged npm run dev. now you cant see js files with ts files

--- routes/item.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createItem, deleteItem, editItem, getItemByRestaurantId } from "../controllers/item.js";
-const router = express.Router();
-router.get('/:restaurantId', getItemByRestaurantId);
-router.post('/create', createItem);
-router.post('/edit', editItem);
-router.post('/delete', deleteItem);
-export default router;
--- schemas/inviteSchemas.js
+++
@@ -1,4 +0,0 @@
-import { z } from 'zod';
-export const inviteAcceptSchema = z.object({
-    teamId: z.string(),
-});
