ba27c35dced0414154c71ede5a4fe162f62baa38
Rehan Peiris <rehanperis58@gmail.com>
2024-09-07 23:34:32 +0530

    Profile fix 7

--- controllers/profile.ts
+++ controllers/profile.ts
@@ -1,93 +1,75 @@
 import Profile from '../models/Profile.js'
-import asyncHandler from 'express-async-handler'
 
 import { RequestHandler } from 'express'
 
-/* const getProfile: RequestHandler = asyncHandler(async (req, res) => {
-    const { userID } = req.params;
-    console.log("Recieved user Id", userID);
-
-    const profile = await Profile.findOne({ userId: userID });
-
-    if (!profile) {
-        res.status(404).json({ message: 'Profile not found' })
-    }
-
-    res.status(200).json(profile)
-}) */
 const getProfile: RequestHandler = async (req, res) => {
     const { userId } = req.params;
-    console.log("Recieved user Id", userId);
     try {
         const profile = await Profile.find({ userId:userId });
         if (!profile) {
-            return res.status(404).json({ message: 'Menu not found' });
+            return res.status(404).json({ message: 'Profile not found' });
         }
         res.status(200).json(profile);
     }
     catch (error) {
-        res.status(500).json({ message: 'Error fetching menu', error });
+        res.status(500).json({ message: 'Error fetching profile', error });
     }
 };
 
 const createNewProfile: RequestHandler = async (req, res) => {
-    const { first_name, last_name, role, email, userId } = req.body
+    const { first_name, last_name, role, email, userId } = req.body;
+    try {
+        const profile = await Profile.create({
+            first_name,
+            last_name,
+            email,
+            role,
+            userId
+        });
 
-    if (!first_name || !last_name || !role) {
-        return res.status(400).json({ message: 'All fields are required' })
+        res.json({
+            profile
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating profile', error });
     }
+};
 
-    const profile = await Profile.create({
-        first_name,
-        last_name,
-        role,
-        email,
-        userId: userId
-    })
+const updateProfile: RequestHandler = async (req, res) => {
+    const { first_name, last_name, role, email, userId } = req.body;
 
-    if (profile) {
-        res.status(201).json({ message: `New profile created` })
-    } else {
-        res.status(400).json({ message: 'Invalid profile data received' })
+    try {
+        const profile = await Profile.findByIdAndUpdate(
+            userId,
+            { first_name, last_name, email, role },
+        );
+
+        if (!profile) {
+            return res.status(404).json({ message: 'Profile not found' });
+        }
+
+        res.status(200).json({ profile });
+    } catch (error) {
+        res.status(500).json({ message: 'Error editing profile', error });
     }
-}
+};
 
-const updateProfile: RequestHandler = asyncHandler(async (req, res) => {
-    const { first_name, last_name, role, email, userId } = req.body
+const deleteProfile: RequestHandler = async (req, res) => {
+    const { userId } = req.body;
 
-    if (!first_name || !last_name || !role) {
-        res.status(400).json({ message: 'All fields are required' })
+    try {
+        const profile = await Profile.findByIdAndDelete(userId);
+
+        if (!profile) {
+            return res.status(404).json({ message: 'Profile not found' });
+        }
+
+        res.json({
+            profile
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting profile', error });
     }
-
-    const profile = await Profile.findByIdAndUpdate(userId, {
-        first_name,
-        last_name,
-        role,
-        email,
-        userId
-    })
-
-    if (profile) {
-        res.status(201).json({ message: `Profile updated` })
-    } else {
-        res.status(400).json({ message: 'Invalid profile data received' })
-    }
-})
-
-const deleteProfile: RequestHandler = asyncHandler(async (req, res) => {
-    const { userId } = req.body
-
-    if (!userId) {
-        res.status(400).json({ message: 'User ID is required' })
-    }
-
-    const profile = await Profile.findByIdAndDelete(userId)
-
-    if (profile) {
-        res.status(201).json({ message: `Profile deleted` })
-    } else {
-        res.status(400).json({ message: 'Invalid profile data received' })
-    }
-})
+};
 
 export { getProfile, createNewProfile, updateProfile, deleteProfile }
